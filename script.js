ee5a2844280f03ea0405656bab0823d8771f6562
unknown <diandraregita14@gmail.com>
2023-11-21 20:55:09 +0700

    Add files

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.btn-color').addEventListener('click', () => calculateBMI());
+  });
+
+function calculateBMI() {
+    // Get user input
+    var weight = document.getElementById("weight").value;
+    var height = document.getElementById("height").value;
+
+    // Validate input
+    if (isNaN(weight) || weight <= 0) {
+      document.getElementById("weight").classList.add("is-invalid");
+      return;
+    } else {
+        document.getElementById("weight").classList.remove("is-invalid");
+    }
+
+    if (isNaN(height) || height <= 0) {
+        document.getElementById("height").classList.add("is-invalid");
+        return;
+    } else {
+        document.getElementById("height").classList.remove("is-invalid");
+    }
+
+    // Calculate BMI
+    var bmi = (weight / ((height / 100) ** 2)).toFixed(2);
+
+    // Determine BMI category
+    var category;
+    if (bmi < 18.5) {
+        category = "Underweight";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        category = "Normal weight";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        category = "Overweight";
+    } else {
+        category = "Obesity";
+    }
+
+    // Display result
+    var resultElement = document.getElementById("result");
+    resultElement.innerHTML = "Your BMI is: " + bmi + "<br>Category: " + category;
+}
